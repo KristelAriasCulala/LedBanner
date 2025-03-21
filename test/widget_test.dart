9030c57f095f166a072f93117276ca974c4e8685
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-03-21 09:55:29 +0800

    Modify app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:led_banner_app/main.dart';
+import 'package:Neonora/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
